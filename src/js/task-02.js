712ca247af109f44c04f4b18d6fa12b9201b560d
1gog1 <110350969+1gog1@users.noreply.github.com>
2022-12-08 13:11:21 +0100

    hw-js-6-complited

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+
+const list = document.querySelector('#ingredients')
+
+for (let i = 0; i < ingredients.length; i += 1) {
+	const listItem = document.createElement('li')
+	listItem.textContent = ingredients[i]
+	listItem.classList.add('item')
+	list.append(listItem)
+}
